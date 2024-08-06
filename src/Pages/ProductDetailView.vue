d2b0d6620a02ffb7ffbfc696a831f7d3c7a0a438
Your Name <you@example.com>
2024-08-06 13:15:52 +0200

    fixed the sorting function

--- src/Pages/ProductDetailView.vue
+++ src/Pages/ProductDetailView.vue
@@ -1,39 +1,53 @@
+<script>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import ProductDetail from '../components/Product/ProductDetail.vue';
+import ProductSkeleton from '../components/Product/ProductSkeleton.vue';
+import Error from '../components/Error.vue';
+
+export default {
+  components: {
+    ProductDetail,
+    ProductSkeleton,
+    Error,
+  },
+  setup() {
+    const route = useRoute();
+    const product = ref(null);
+    const loading = ref(true);
+    const error = ref(null);
+
+    onMounted(async () => {
+      try {
+        const response = await fetch(`https://fakestoreapi.com/products/${route.params.id}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch product');
+        }
+        product.value = await response.json();
+      } catch (err) {
+        error.value = err.message;
+      } finally {
+        loading.value = false;
+      }
+    });
+
+    return {
+      product,
+      loading,
+      error,
+    };
+  },
+};
+</script>
+
 <template>
-    <div class="flex justify-center">
-      <Error v-if="error" :message="error.message" />
-      <ProductSkeleton v-if="loading && !error" />
-      <ProductDetail v-else-if="!loading && !error" v-bind="product" />
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref, onMounted } from 'vue';
-  import { useRoute } from 'vue-router';
-  import ProductDetail from '../components/Product/ProductDetail.vue';
-  import ProductSkeleton from '../components/Product/ProductSkeleton.vue';
-  import Error from '../components/Error.vue';
-  import { fetchSingleProduct } from '../api/api';
-  
-  const route = useRoute();
-  const id = route.params.id;
-  
-  const product = ref({});
-  const error = ref(null);
-  const loading = ref(false);
-  
-  onMounted(async () => {
-    loading.value = true;
-    const { response, error: fetchError } = await fetchSingleProduct(id);
-    if (fetchError) {
-      error.value = fetchError;
-    } else {
-      product.value = response;
-    }
-    loading.value = false;
-  });
-  </script>
-  
-  <style scoped>
-  /* Add styles here */
-  </style>
-  
+  <div>
+    <Error v-if="error" :message="error" />
+    <ProductSkeleton v-else-if="loading" />
+    <ProductDetail v-else :product="product" />
+  </div>
+</template>
+
+<style scoped>
+/* Add styles here */
+</style>
